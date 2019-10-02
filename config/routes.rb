438945c28ce15a18f342ed315f409b2b6fc5266c
Paul Kogan <paulkog@gmail.com>
2019-10-02 18:07:13 -0400

    custom routes - 2 ways

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  #get 'pages/home'
-  #get 'pages/about'
-  #get 'pages/contact'
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+ 
+  get 'about', to: 'pages#about'
+  get 'aboutme', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   #get 'portfolios/new'
   resources :blogs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #set the root path
+  root to: 'pages#home'
 end
