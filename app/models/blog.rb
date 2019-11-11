37b3961e375dbc3cbc17cd7b3bd22ad952964b5c
paulkogan <paulkog@gmail.com>
2019-11-10 20:48:24 -0500

    validates fields - no blank

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,5 @@ class Blog < ApplicationRecord
     enum status: {draft: 0, approved: 1, published: 2}
     extend FriendlyId
     friendly_id :title, use: :slugged
+    validates_presence_of :title, :body
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,3 @@
 class Portfolio < ApplicationRecord
+    validates_presence_of :title, :body, :thumb_img, :main_img
 end
